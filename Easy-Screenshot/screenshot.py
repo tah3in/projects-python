ab0b742c56bbd3374d8686d0085eb30a937d854b
tah3in <tah3in@gmail.com>
2022-09-06 20:11:26 +0430

    First commit Easy-Screenshot

---
+++ Easy-Screenshot/screenshot.py
@@ -0,0 +1,18 @@
+from PIL import ImageGrab
+import sys , os
+
+listorg=[]
+print(os.listdir())
+for i in os.listdir():
+    try:
+        i,Format=i.split(".")
+        listorg.append(int(i))
+    except:
+        continue
+
+for i in range(10000000):
+    if i not in listorg:
+        sc = ImageGrab.grab()
+        sc.save(f"{i}.png")
+        break
+
