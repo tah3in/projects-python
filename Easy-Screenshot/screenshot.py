61821ac401aed8f3e96c5a4c80c3754d81da142e
mOsTafa <tah3in@gmail.com>
2023-09-16 00:39:13 +0330

    add time to project easy_screenshot

--- Easy-Screenshot/screenshot.py
+++ Easy-Screenshot/screenshot.py
@@ -1,5 +1,16 @@
 from PIL import ImageGrab
+import time
 import sys , os
+current_time = time.localtime()
+
+year = current_time.tm_year
+month = current_time.tm_mon
+day = current_time.tm_mday
+hour = current_time.tm_hour
+minute = current_time.tm_min
+second = current_time.tm_sec
+
+print()
 
 listorg=[]
 print(os.listdir())
@@ -9,10 +20,6 @@ for i in os.listdir():
         listorg.append(int(i))
     except:
         continue
-
-for i in range(10000000):
-    if i not in listorg:
-        sc = ImageGrab.grab()
-        sc.save(f"{i}.png")
-        break
+sc = ImageGrab.grab()
+sc.save(f"{year}{month}{day}_{hour}{minute}{second}.png")
 
